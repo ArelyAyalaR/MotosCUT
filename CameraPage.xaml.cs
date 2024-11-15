0ddaf613eb80301d252028d07315c18359e37896
Alex <129354257+AmethystMovie14@users.noreply.github.com>
2024-11-15 11:05:05 -0600

    Test

--- CameraPage.xaml.cs
+++ CameraPage.xaml.cs
@@ -1,9 +1,29 @@
+using ZXing.Net.Maui;
+
 namespace ProyectoMotos;
 
 public partial class CameraPage : ContentPage
 {
-	public CameraPage()
-	{
-		InitializeComponent();
-	}
+    public CameraPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnBarcodeDetected(object sender, BarcodeDetectionEventArgs e)
+    {
+        var result = e.Results.FirstOrDefault()?.Value;
+
+        if (!string.IsNullOrEmpty(result))
+        {
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Código Detectado", $"Contenido: {result}", "OK");
+                BarcodeReader.IsScanning = false;
+            });
+        }
+    }
+    private void OnStopScanningClicked(object sender, EventArgs e)
+    {
+        BarcodeReader.IsScanning = false;
+    }
 }
